15b418ff1393ff4221af85415588968014f52f3b
Minka5 <madrakowsky5@gmail.com>
2018-07-26 15:04:09 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -1,10 +1,10 @@
 # Your methods should take in a hash as an argument - here's an example of what the hash will look like - the methods should take this hash as an argument and return the desired output. 
 
-# weather = {
-# 	:city => "New York",
-# 	:description => "Partly Cloudy",
-# 	:temperature_farenheit => 66.3
-# }
+weather = {
+	:city => "New York",
+	:description => "Partly Cloudy",
+	:temperature_farenheit => 66.3
+}
 
 #hash[key] = value 
 
